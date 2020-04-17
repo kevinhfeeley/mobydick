76c67209ab9afb85163d608decc0d90439d21a01
kevinhfeeley <49758899+kevinhfeeley@users.noreply.github.com>
2020-04-17 13:06:40 -0500

    Updated file to include "stops" functionality.

--- mobydick.php
+++ mobydick.php
@@ -1,13 +1,16 @@
 <?php
-	$moby = file_get_contents('https://www.gutenberg.org/files/2701/2701-h/2701-h.htm');
+
+	$stops = file_get_contents('./stop-words.txt');
+	$stops = explode("\n",$stops);
+	$moby = file_get_contents('./mobydick.txt');
 	echo "<pre>";
 	$moby = explode("\n",$moby);
 	$wordlist=array();
 	$punct=array('.',',','"',"'",";","!","“","”","’s","—","(",")","’","?");
 	foreach($moby AS $line){
-		$words = explode(" ",strip_tags(str_replace($punct,'',html_entity_decode(trim(strtolower($line))))));
+		$words = explode(" ",str_replace($punct,'',trim(strtolower($line))));
 		foreach($words AS $word){
-			if(strlen(trim($word))>0){
+			if(strlen(trim($word))>0&&!in_array(trim($word),$stops)){
 				if(isset($wordlist[trim($word)])){
 					$wordlist[trim($word)]++;
 				}else{
@@ -20,6 +23,7 @@
 	$max = max($wordlist);
 	echo "Text pulled from https://www.gutenberg.org/files/2701/2701-h/2701-h.htm<br />";
 	echo "<table><tr><th style='background-color:#999;'>Word</th><th style='background-color:#999;'>Count</th><th style='background-color:#999;'>&nbsp;</th></tr>";
+	$i=0;
 	foreach($wordlist AS $word=>$count){
 		echo "<tr><th>";
 		echo $word."</th><th>";
@@ -27,7 +31,8 @@
 		echo "</th><td>";
 ?><img src='./mobygraph.php?max=<?php echo $max; ?>&count=<?php echo $count; ?>' /><?php
 		echo "</td></tr>";
+		$i++;
+		if($i==10){break;}
 	}
 	echo "</table>";
-//	print_r($wordlist);
 ?>
